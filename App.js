e9546516c3c5755e112d56bdc988db5b740d88ba
JennyTLam <jennytlam18@gmail.com>
2020-05-11 18:06:53 -0500

    link static tree data

--- App.js
+++ App.js
@@ -11,23 +11,27 @@ import {
 } from 'react-native';
 import React, {useState} from 'react';
 import TreeCircle from './src/TreeCircle';
+import treesData from './src/TreesData.json';
+import TreeGrid from './src/TreeGrid';
+
 //import all the components we will need
 
-const App = () => {
-  const [items, setItems] = useState([1,3,3,3,3,7,5,5,1243,2,42,34,32,412,42,134,123,42,4,2134124,213,432,4,34,3,4,3,4,34,34,3,412,3,4,34,3,4,34,4,3,42,42134,]);
+function StackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'My home' }}
+      />
+    </Stack.Navigator>
+  );
+}
 
+const App = () => {
   return (
     <View style={styles.MainContainer}>
-      <FlatList
-        columnWrapperStyle={{justifyContent:'center'}}
-        data={items}
-        renderItem={({ item }) => (
-          <TreeCircle></TreeCircle>
-        )}
-        //Setting the number of column
-        numColumns={2}
-        keyExtractor={(item, index) => index.toString()}
-      />
+      <TreeGrid treesData={treesData}/>
     </View>
   );
 }
--- src/TreeCircle.js
+++ src/TreeCircle.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import treesData from "./TreesData.json";
 
 const styles = StyleSheet.create({
     container: {
@@ -8,21 +7,40 @@ const styles = StyleSheet.create({
       alignItems: "center",
       justifyContent: "center",
     },
-    circle: {
-        width: 150,
-        height: 150,
-        borderRadius: 75,
-        margin: 10,
-        backgroundColor: 'red'
+    unhealthy_circle: {
+      width: 150,
+      height: 150,
+      borderRadius: 75,
+      margin: 10,
+      backgroundColor: 'red',
+      justifyContent: 'center', 
      },
+     healthy_circle: {
+      width: 150,
+      height: 150,
+      borderRadius: 75,
+      margin: 10,
+      backgroundColor: 'green',
+      justifyContent: 'center', 
+     },
+     name: {
+      textAlign: "center",
+      fontSize: 25
+     }, 
+      title: {
+      textAlign: "center",
+      fontSize: 10
+     }
   });  
 
 const TreeCircle = ({treeData}) => {    
   return (
     <View style={styles.container}>
-        <View style={styles.circle}>
-            <Text>I AM NOT A TREEE</Text>
+        <View style={treeData.HI > 5 ? styles.healthy_circle : styles.unhealthy_circle}>
+          <Text style={styles.title}> Health Index (HI) </Text>
+          <Text style={styles.name}> {treeData.HI} </Text>
         </View>
+        <Text style={styles.name}>{treeData.name}</Text>
     </View>
   );
 }
