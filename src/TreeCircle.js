3a644833247190248ceaa653d7565dadd0ddb10f
JennyTLam <jennylam2021@u.northwestern.edu>
2020-05-25 18:31:17 -0500

    Updated dashboard UI to have profile picture JZ FY YW

--- src/TreeCircle.js
+++ src/TreeCircle.js
@@ -45,6 +45,7 @@ const styles = StyleSheet.create({
 const TreeCircle = ({ treeData, navigation }) => {
   return (
     <View style={styles.container}>
+
       <TouchableOpacity
         style={
           (treeData.health == 0) ? styles.healthy_circle : (treeData.health == 1 ? styles.warning_circle : styles.unhealthy_circle)
@@ -58,112 +59,49 @@ const TreeCircle = ({ treeData, navigation }) => {
 
       <View>
         <React.Fragment>
-        <Image  style={{ width: 50, height: 50 }} source={require('./healthy_tree.png')} /> 
-        <Avatar 
-          icon={treeData.health === 0 ? {name:'check-circle',
-          type:'material-icons',color:'green'} :
-          treeData.health === 1 ? {name:'warning',
-          type:'material-icons', color:'rgb(255,204,51)'} :
-          {name:'error',
-          type:'material-icons',
-          color:'rgb(228,66,4)'}
-          }
-          rounded
-          size='large'
-          containerStyle={{ position: 'absolute', top: -70, right: 5}}
-        />
-        <View>
-        <IconBadge 
-          // MainElement={
-          //   <View style={{backgroundColor:'#489EFE',
-          //     width:50,
-          //     height:50,
-          //     margin:6
-          //   }}/>
-          // }
-          BadgeElement={
+          <Image  style={{ width: 150, height: 150, borderRadius: 75}} source={require("./testtree.png")} /> 
 
-            //<Icon name='tree' type='entypo' size="60"></Icon>
-            treeData.health === 0 ? <Image  style={{ width: 50, height: 50 }} source={require('./healthy_tree.png')} /> :
-            (treeData.health === 1 ?  <Image  style={{ width: 50, height: 50 }} source={require('./declining_tree.png')} /> :
-                                      <Image  style={{ width: 50, height: 50 }} source={require('./dead_tree.png')} /> )
+          <View>
+            <IconBadge 
+              BadgeElement={
 
-          }
-          IconBadgeStyle={
-            {width:60,
-            height:70,
-            backgroundColor: treeData.health === 0 ? "rgb(188,213,184)" : treeData.health === 1 ? "rgba(239,223,180,255)" : "rgb(234,170,156)",
-            marginRight:45,
-            marginTop:-20}
-          }
-          // Hidden={this.state.BadgeCount==0}
-        />
-        </View>
+                //<Icon name='tree' type='entypo' size="60"></Icon>
+                treeData.health === 0 ? <Image  style={{ width: 40, height: 40 }} source={require('./healthy_tree.png')} /> :
+                (treeData.health === 1 ?  <Image  style={{ width: 40, height: 40 }} source={require('./declining_tree.png')} /> :
+                                          <Image  style={{ width: 40, height: 40 }} source={require('./dead_tree.png')} /> )
+              }
+              IconBadgeStyle={
+                {width:60,
+                height:60,
+                borderRadius: 30,
+                backgroundColor: treeData.health === 0 ? "rgb(188,213,184)" : treeData.health === 1 ? "rgba(239,223,180,255)" : "rgb(234,170,156)",
+                marginRight:0,
+                marginTop:-150}
+              }
+            />
+          </View>
+          {/*<Avatar 
+            icon={treeData.health === 0 ? {name:'check-circle',
+            type:'material-icons',color:'green'} :
+            treeData.health === 1 ? {name:'warning',
+            type:'material-icons', color:'rgb(255,204,51)'} :
+            {name:'error',
+            type:'material-icons',
+            color:'rgb(228,66,4)'}
+            }
+            rounded
+            size='large'
+            containerStyle={{ position: 'absolute', top: -30, right: -30}}
+          />*/}
+        </React.Fragment>
       </View>
       </TouchableOpacity>
       <Text style={styles.name}>
         {treeData.name}
       </Text>
-      </React.Fragment>
     </View>
-    {/*<View style={styles.container}>
-      <TouchableOpacity
-        style={
-          (treeData.health == 0) ? styles.healthy_circle : (treeData.health == 1 ? styles.warning_circle : styles.unhealthy_circle)
-        }
-        onPress={() =>
-          navigation.navigate("TreeSummary", {
-            treedata: treeData,
-          })
-        }
-      >
-      <View>
-        <Avatar 
-          icon={treeData.health === 0 ? {name:'check-circle',
-          type:'material-icons',color:'green'} :
-          treeData.health === 1 ? {name:'warning',
-          type:'material-icons', color:'rgb(255,204,51)'} :
-          {name:'error',
-          type:'material-icons',
-          color:'rgb(228,66,4)'}
-          }
-          rounded
-          size='large'
-          containerStyle={{ position: 'absolute', top: -70, right: 5}}
-        />
-        <View>
-        <IconBadge 
-          // MainElement={
-          //   <View style={{backgroundColor:'#489EFE',
-          //     width:50,
-          //     height:50,
-          //     margin:6
-          //   }}/>
-          // }
-          BadgeElement={
-
-            //<Icon name='tree' type='entypo' size="60"></Icon>
-            treeData.health === 0 ? <Image  style={{ width: 50, height: 50 }} source={require('./healthy_tree.png')} /> :
-            (treeData.health === 1 ?  <Image  style={{ width: 50, height: 50 }} source={require('./declining_tree.png')} /> :
-                                      <Image  style={{ width: 50, height: 50 }} source={require('./dead_tree.png')} /> )
-
-          }
-          IconBadgeStyle={
-            {width:60,
-            height:70,
-            backgroundColor: treeData.health === 0 ? "rgb(188,213,184)" : treeData.health === 1 ? "rgba(239,223,180,255)" : "rgb(234,170,156)",
-            marginRight:45,
-            marginTop:-20}
-          }
-          // Hidden={this.state.BadgeCount==0}
-        />
-        </View>
-      </View>
-      </TouchableOpacity>
-      <Text style={styles.name}>
-        {treeData.name}
-      </Text>
-    </View>*/}
+    
+  
   );
 };
 
